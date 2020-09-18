abf4c2cbbd9080f0cba204748b6b3109f76d63ad
pltrue <pl1998@users.noreply.github.com>
2020-09-18 05:36:11 +0000

    Apply fixes from StyleCI

--- src/Handle/Handle.php
+++ src/Handle/Handle.php
@@ -1,13 +1,15 @@
 <?php
-/**
- * Created by PhpStorm
- * User: pl
- * Date: 2020/9/18
- * Time: 11:48
+
+/*
+ * This file is part of the pl1998/thirdparty_oauth.
+ *
+ * (c) pl1998<pltruenine@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Pl1998\ThirdpartyOauth\Handle;
-
 
 interface Handle
 {
