b67cb3904890ab054799d9ca16c3083a69397a21
上海明馨科技有限公司 <admin@mxin.ltd>
2020-12-19 23:03:37 +0800

    Update DouyinOauth.php

--- src/Handle/DouyinOauth.php
+++ src/Handle/DouyinOauth.php
@@ -17,9 +17,10 @@ class DouyinOauth implements Handle
 {
     protected $client;
     protected $config;
-    protected $authorization_url='https://open.douyin.com/platform/oauth/connect/';
-    protected $token_url='https://open.douyin.com/oauth/access_token/';
-    protected $userinfo_url='https://open.douyin.com/oauth/userinfo/';
+    protected $authorization_url = 'https://open.douyin.com/platform/oauth/connect/';
+    protected $token_url         = 'https://open.douyin.com/oauth/access_token/';
+    protected $userinfo_url      = 'https://open.douyin.com/oauth/userinfo/';
+
     public function __construct($config)
     {
         $this->config = $config;
@@ -28,67 +29,73 @@ class DouyinOauth implements Handle
 
     public function authorization()
     {
-        //$url = 'https://graph.qq.com/oauth2.0/authorize';
+
         $query = array_filter([
             'response_type' => 'code',
-            'client_key' => $this->config['client_id'],
-            'redirect_uri' => $this->config['redirect_uri'],
-            'scope' => '',
-               'state' => 'https://6.mxin.ltd/login/qqcallback',
+            'client_key'    => $this->config['client_id'],
+            'redirect_uri'  => $this->config['redirect_uri'],
+            'scope'         => '',
+            'state'         => 'https://6.mxin.ltd/login/qqcallback',
         ]);
 
-        $url = $this->authorization_url.'?'.http_build_query($query);
+        $url = $this->authorization_url . '?' . http_build_query($query);
 
-        header('Location:'.$url);
+        header('Location:' . $url);
         exit();
     }
 
     public function getAccessToken()
     {
-       // $url = 'https://graph.qq.com/oauth2.0/token?grant_type=authorization_code';
 
         $query = array_filter([
-            'client_key' => $this->config['client_id'],
-            'code' => $_GET['code'],
-            'grant_type' => 'authorization_code',
+            'client_key'    => $this->config['client_id'],
+            'code'          => $_GET['code'],
+            'grant_type'    => 'authorization_code',
             'client_secret' => $this->config['client_secret'],
-            'redirect_uri' => $this->config['redirect_uri'], 
-           
+            'redirect_uri'  => $this->config['redirect_uri'],
+
         ]);
 
-         
-$res=json_decode($this->client->request('get', $this->token_url, [
+
+        $res = json_decode($this->client->request('get', $this->token_url, [
             'query' => $query,
         ])->getBody()->getContents())->data;
-        $this->openid=$res->open_id;
-          $this->unionid=$res->unionid;
-        
-        return  $res->access_token;
-        exit;
-        
-    
+        if (isset($res->access_token)) {
+            $this->openid  = $res->open_id;
+            $this->unionid = $res->unionid;
 
-    
+            return $res->access_token;
+        } else {
+            exit("获取 ACCESS_TOKEN 出错：" . $res);
+        }
+
+        return $res->access_token;
+        exit;
+
+
     }
 
-    public function getUserInfo($access_token)
+    public function getUserInfo($access_token): object
     {
-      
+
         $query = array_filter([
-            'open_id' => $this->openid,
-          
+            'open_id'      => $this->openid,
             'access_token' => $access_token,
         ]);
-       
-        $userinfo = json_decode($this->client->request('GET',  $this->userinfo_url, [
+
+        $userinfo = json_decode($this->client->request('GET', $this->userinfo_url, [
             'query' => $query,
         ])->getBody()->getContents())->data;
+        if (isset($userinfo->>open_id)) {
+            $userinfo->openid  = $userinfo->open_id;
+            $userinfo->unionid = $userinfo->union_id;
+        } else {
+            die('获取用户信息失败');
+        }
 
-      $userinfo->openid= $userinfo->open_id;
-        $userinfo->unionid= $userinfo->union_id;
 
         return $userinfo;
     }
 
-  
+
 }
